727b3d9e54849a620e3a3a04d71e438093d1bdea
ngoquy12 <ngoq753429@gmail.com>
2024-04-23 10:44:46 +0700

    update file

--- src/pages/client/review/CreateReview.jsx
+++ src/pages/client/review/CreateReview.jsx
@@ -42,7 +42,7 @@ export default function CreateReview() {
               </label>
               <Rating defaultValue={0} />
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center mb-[28px]">
               <label
                 className="min-w-[200px] text-[17px] uppercase font-semibold"
                 htmlFor=""
@@ -96,6 +96,162 @@ export default function CreateReview() {
                   ></div>
                 </div>
               </div>
+            </div>
+            <div className="flex items-center mb-[28px]">
+              <label
+                className="min-w-[200px] text-[17px] uppercase font-semibold"
+                htmlFor=""
+              >
+                Tiêu đề *
+              </label>
+              <div className="w-full">
+                <input
+                  type="text"
+                  className="login-input w-full"
+                  placeholder="Tóm tắt đánh giá của bạn"
+                />
+                <p>Tiêu đề của bạn phải ít hơn 100 ký tự.</p>
+              </div>
+            </div>
+            <div className="flex items-center mb-[28px]">
+              <label
+                className="min-w-[200px] text-[17px] uppercase font-semibold"
+                htmlFor=""
+              >
+                Bình luận *
+              </label>
+              <div className="w-full">
+                <textarea className="login-input" cols="30" rows="3"></textarea>
+                <p>Bạn phải viết ít nhất 50 ký tự tại đây.</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 mb-[28px]">
+              <label
+                className="min-w-[200px] text-[17px] uppercase font-semibold"
+                htmlFor=""
+              >
+                Kích cỡ đã mưa *
+              </label>
+              <div className="w-full">
+                <select className="login-input">
+                  <option value="">1</option>
+                  <option value="">1</option>
+                  <option value="">1</option>
+                  <option value="">1</option>
+                </select>
+              </div>
+            </div>
+            <div className="flex items-center mb-[28px]">
+              <label
+                className="min-w-[200px] text-[17px] uppercase font-semibold"
+                htmlFor=""
+              >
+                Biệt danh *
+              </label>
+              <div className="w-full">
+                <input
+                  type="text"
+                  className="login-input"
+                  placeholder="Vui lòng nhập tên đăng nhập"
+                />
+                <p>Vui lòng nhập tên đăng nhập.</p>
+              </div>
+            </div>
+            <h3 className="uppercase font-bold text-[20px] my-[20px]">
+              Thông tin người mặc
+            </h3>
+            <div className="grid grid-cols-2 gap-[50px] mb-[28px]">
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Giới tính *</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Vị trí *</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[50px] mb-[28px]">
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Độ tuổi</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Chiều cao</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-[50px] mb-[28px]">
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Cân nặng</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold uppercase">Cỡ giày</label>
+                <select className="login-input">
+                  <option value="">Chọn</option>
+                  <option value="">Nam</option>
+                  <option value="">Nữ</option>
+                  <option value="">Khác</option>
+                  <option value="">Câu trả lời khác</option>
+                </select>
+              </div>
+            </div>
+            <div className="border-b my-[20px]"></div>
+            <h3 className="uppercase font-bold text-[20px]">
+              ĐĂNG BÀI ĐÁNH GIÁ CỦA BẠN:
+            </h3>
+            <ul className="ml-[28px]">
+              <li className="list-disc">
+                Bình luận đã đăng của bạn có thể được sử dụng cho mục đích quảng
+                cáo.
+              </li>
+              <li className="list-disc">
+                Chúng tôi không chấp nhận các yêu cầu đăng quảng cáo cho các
+                thương hiệu, cá nhân, tổ chức khác. Ngoài ra, chúng tôi không
+                thể đăng tình trạng còn hàng và kích cỡ sản phẩm.{" "}
+              </li>
+            </ul>
+            <div className="my-[20px] flex items-center gap-2">
+              <input className="h-5 w-5" type="checkbox" />
+              <span>Tôi đồng ý với ĐIỀU KHOẢN SỬ DỤNG CỦA UNIQLO</span>
+            </div>
+            <div className="flex items-center gap-4 my-[20px]">
+              <button class="h-[45px] text-[16px] uppercase font-bold hover:opacity-70 bg-[#000000] text-white w-[288px] py-2 px-1">
+                Gửi
+              </button>
+              <button class="h-[45px] text-[16px] uppercase font-bold hover:opacity-70 border border-[#7d7d7d] w-[288px] py-2 px-1">
+                Quay lại chi tiết sản phấm
+              </button>
             </div>
           </section>
           <article className="flex-1 h-[365px] w-[365px]">
